bb416c657e3f05e29a726590abde4093ed726087
Nitesh <niteshtiwari0009@gamil.com>
2026-06-17 16:25:33 +0530

    Fixed Gemini AI service and interview report generation

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,11 @@
 require("dotenv").config()
 const app = require("./src/app.js")
 const connectTODB = require("./src/config/database")
-const invokeGeminiAi = require("./src/services/ai.service")
-
-
+const {resume, selfDescription, jobDescription } = require("./src/services/temp")
+const  generateInterviewReport = require("./src/services/ai.service")
 connectTODB()
-invokeGeminiAi()
 
+generateInterviewReport({resume, selfDescription, jobDescription })
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
